18b1e782cac7fb4ededbbc2831913966fd1cec5d
anuarnurshinov <anuarnurshinov@gmail.com>
2022-10-12 19:09:01 +0300

    Добавлен контроллер пользователей

---
+++ src/users/users.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express"
+import { BaseController } from "../common/base.controller"
+import { LoggerService } from "../logger/logger.service"
+
+export class UserController extends BaseController {
+  constructor(logger: LoggerService) {
+    super(logger)
+    this.bindRoutes([
+      {
+        path: "/register",
+        method: "post",
+        function: this.register,
+      },
+      {
+        path: "/login",
+        method: "post",
+        function: this.login,
+      },
+    ])
+  }
+
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "login")
+  }
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "register")
+  }
+}
